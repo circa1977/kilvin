e0b028e90142e6d500abe489d5c8e172c1838bbc
Paul Burdick <paul@reedmaniac.com>
2017-08-10 10:12:12 -0700

    Installer is exempt from CSRF tokens?

--- cms/app/Http/Middleware/VerifyCsrfToken.php
+++ cms/app/Http/Middleware/VerifyCsrfToken.php
@@ -14,4 +14,30 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+
+    /**
+     * Determine if the request has a URI that should pass through CSRF verification.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function inExceptArray($request)
+    {
+    	if (REQUEST == 'INSTALL') {
+    		return true;
+    	}
+
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+
+            if ($request->is($except)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
